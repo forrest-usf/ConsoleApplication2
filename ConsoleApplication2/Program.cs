94ba22fb344ca3c5f6cc7a1f28ce8dd562494b26
forrest-usf <forrest@mail.usf.edu>
2019-01-31 17:59:30 -0500

    version1

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -4,12 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication2
+namespace ConsoleApplication1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.Write("What is your name? ");
+            string input = Console.ReadLine();
+
+            try
+            {
+                Console.WriteLine("Hello " + input + "!");
+                Console.WriteLine("Press any key to continue . . . ");
+                Console.ReadKey(true);
+            }
+            catch
+            {
+                Console.WriteLine("An error has occured.");
+                Console.ReadKey(true);
+            }
         }
     }
 }
